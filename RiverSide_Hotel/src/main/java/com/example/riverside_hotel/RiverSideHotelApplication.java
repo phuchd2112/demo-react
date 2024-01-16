6ee3085fecbb68575a002a708a147b0032e68d84
DESKTOP-G27BKBP\phuch <phuchd2112@gmail.com>
2024-01-16 22:06:19 +0700

    init

---
+++ RiverSide_Hotel/src/main/java/com/example/riverside_hotel/RiverSideHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.riverside_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiverSideHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiverSideHotelApplication.class, args);
+	}
+
+}
---
+++ RiverSide_Hotel/src/test/java/com/example/riverside_hotel/RiverSideHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riverside_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverSideHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
